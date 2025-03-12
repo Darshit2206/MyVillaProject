6327c7ac573749b4f7b11b8c6183b69ec5128535
Darshit <darshitpatel2203@gmail.com>
2025-03-12 19:18:02 +0530

    updated Villa Availability section

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Web.ViewModels;
 
@@ -22,6 +23,32 @@ namespace WhiteLagoon.Web.Controllers
             };
             return View(homeVM);
         }
+        [HttpPost]
+        public IActionResult Index(HomeVM homeVM)
+        {
+            homeVM.VillaList = _unitOfWork.Villa.GetAll(incProps: "VillaAmenity");  
+            return View(homeVM);
+        }
+
+        public IActionResult GetVillasByDate(int night, DateOnly checkInDate)
+        {
+            var villaList = _unitOfWork.Villa.GetAll(incProps: "VillaAmenity").ToList();
+            foreach (var villa in villaList)
+            {
+                if (villa.Id % 2 == 0)
+                {
+                    villa.IsAvailable = false;
+                }
+            }
+            HomeVM homeVM = new()
+            {
+                CheckInDate = checkInDate,
+                VillaList = villaList,
+                Nights = night,
+            };
+            return PartialView("_VillaList", homeVM);
+        }
+
         public IActionResult Privacy()
         {
             return View();
